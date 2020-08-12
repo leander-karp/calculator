65220e7bfc9e22551bf2a0c1efccef84848212c0
Leander Karp <leander.karp@invision.de>
2020-08-12 16:16:38 +0200

    Created Base Operator Decorator

--- calculator.js
+++ calculator.js
@@ -1,13 +1,35 @@
-function Operator(a,b){
-    this.a = a; 
-    this.b = b;
-}
-
-Operator.prototype.evaluate = function(){
-    if (typeof(this.a) == "Operator"){
-
+function createOperator(operator){
+    function setOperands(firstOperand="Missing Operand", secondOperand="Missing Operand"){
+        function callOperator(){
+            /*
+            */
+            // function: another operator
+            // !number: Error
+            // otherwise operate
+            let a = firstOperand; 
+            let b = secondOperand;
+            if (typeof firstOperand=="function"){
+                a = firstOperand();
+            }
+            else if (typeof firstOperand!= "number"){
+                return firstOperand;
+            }
+            if (typeof secondOperand=="function"){
+                b = secondOperand();
+            }
+            else if (typeof secondOperand!="number"){
+                return secondOperand;
+            }
+            return operator(a, b);
+        }
+        return callOperator;
     }
+    return setOperands;
 }
 
-console.log(Operator(1,2));
-console.log(typeof(Operator(1,3)));
+const add = createOperator((a,b) => a+b);
+const subtract = createOperator((a,b) => a-b);
+const divide = createOperator((a,b) => {
+    return (b==0)?"ZeroDivisionError":a/b;
+});
+console.log(add(1,2)());
