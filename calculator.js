8914e09de131a827fd98d69e86536d07dcf583a1
Leander Karp <leander.karp@invision.de>
2020-08-20 09:48:29 +0200

    first functional calculator

--- calculator.js
+++ calculator.js
@@ -1,286 +1,113 @@
-class Base{
-    constructor(precedence){
-        this.precedence = precedence;
-    }
-
-    evaluate(){
-        return this;
-    }
-}  
-
-class Failure extends Base {
+class Failure{
     constructor(msg){
-        super(-1);
         this.message = msg;
     }
 }
 
-class Operand extends Base{
-    constructor(content){
-        super(0);
-        this.content = content;
-    }
-    evaluate(){
-        return this.content;
-    }
-}
-
-class Operator extends Base{    
-    constructor(precedence, left=null, right=null){
-        super(precedence);
-        this.left = left;
-        this.right = right;
-    }
-
-    evaluate(evalFunction) {
-        if (this.right == null || this.left == null){
-            return new Failure("Invalid Expression");
-        }
-        else {
-            const left = this.left.evaluate();
-            const right = this.right.evaluate();
-
-            if (right instanceof Failure){
-                return right;
-            }
-            else if (left instanceof Failure){
-                return left;
-            }
-            else {
-                return evalFunction(left, right);
-            }
-    
-        }
-    }
-}
-
-class BinaryOperator extends Operator {
-}
-
-class UnaryOperator extends Operator { 
-    constructor(precedence, left=null){
-        super(precedence, left);
-    }
-
-    evaluate(evalFunction) {
-        if (this.left == null){
-            return new Failure("Invalid Expression");
-        }
-        else {
-            const left = this.left.evaluate();
-            if (left instanceof Failure){
-                return left;
-            }
-            else {
-                return evalFunction(left);
-            }
-        }
-    }
-}
-
-class AddOperator extends BinaryOperator {
-    constructor(left=new Operand(0), right=null){
-        super(1, left, right);
-    }
-    evaluate(){
-        return super.evaluate((a,b) => a+b);
-    }
-}
-
-class SubtractOperator extends BinaryOperator {
-    constructor(left=new Operand(0), right=null){
-        super(1, left, right);
-    }
-    evaluate(){
-        return super.evaluate((a,b) => a-b);
-    }
-}
-
-class DivideOperator extends BinaryOperator {
-    constructor(left=null, right=null){
-        super(2, left, right);
-    }
-    evaluate(){
-        return super.evaluate((a,b) => b!=0? a/b:new Failure("ZeroDivisionError"));
-    }
-}
-
-class MultiplyOperator extends BinaryOperator {
-    constructor(left=null, right=null){
-        super(2, left, right);
-    }
-    evaluate(){
-        return super.evaluate((a,b) => a*b);
-    }
-}
-
-class PowOperator extends BinaryOperator  {
-    constructor(left=null, right=null){
-        super(3, left, right);
-    }
-    evaluate(){
-        return super.evaluate((a,b) => a**b);
-    }
-}
-
-class PlusOperator extends UnaryOperator{
-    evaluate(){
-        return super.evaluate((a) => a);
-    }
-}
-
-class MinusOperator extends UnaryOperator{
-    evaluate(){
-        return super.evaluate((a) => -a);
-    }
-}
-
 const binaryOperators = {
-    "+": AddOperator,
-    "-": SubtractOperator,
-    "/": DivideOperator,
-    "^": PowOperator,
-    "*": MultiplyOperator, 
+    "*": (a,b) => a*b,
+    "-": (a,b) => a-b,
+    "+": (a,b) => a+b,
+    "^": (a,b) => a**b,
+    "/": (a,b) => b!=0?a/b:new Failure("ZeroDivisionError"),
+}
+
+const binaryOperatorPrecedence = {
+    "*": 1,
+    "-": 0,
+    "+": 0,
+    "^": 3,
+    "/": 1,
 }
 
 const unaryOperators = {
-    "-": MinusOperator,
-    "+": PlusOperator,
+    "-": (a) => -a,
+    "+": (a) => a,
 }
 
-function tokenize(inputExpression){
-    let parsedNumber = "";
-    let tokens = [];
+const unaryOperatorsPrecedence = {
+    "-": 2,
+    "+": 2,
+}
 
-    for (let i = 0; i < inputExpression.length; i++) {
-        const token = inputExpression[i];
-        if (token in binaryOperators || token.match(/\s/)){
-            if (parsedNumber){
-                tokens.push(parsedNumber);
-                parsedNumber = "";
-            }
-            if (token in unaryOperators && (inputExpression[i+1] in binaryOperators|| i==0)){
-                tokens.push(new unaryOperators[token]());
-            }
-            else if (token in binaryOperators){
-                tokens.push(new binaryOperators[token]());
-            }
+function solve(expression){
+    /*
+    Inspired by:
+    http://www.martinbroadhurst.com/shunting-yard-algorithm-in-python.html    
+    http://www.reedbeta.com/blog/the-shunting-yard-algorithm/
+    http://wcipeg.com/wiki/Shunting_yard_algorithm
+    */
+    function is_Numerical(tokenToTest){
+        return !isNaN(Number(tokenToTest));
+    }
+    function peek(stack){
+        return stack.length!=0?stack[stack.length-1]:null;
+    }
+    function execute(operator, values){
+        operator = binaryOperators[operator];
+        if (operator.length == 2){
+            const right = values.pop();
+            const left = values.pop();
+            values.push(operator(left, right));
         }
-        else if (token.match(/[0-9\.]+/)){
-            parsedNumber += token;
-
-            if (i == inputExpression.length-1){
-                tokens.push(parsedNumber);
-            }
+        else if (operator.length == 1) {
+            const left = values.pop();
+            values.push(operator(left));
         }
         else {
-            tokens.push(new Failure("Invalid Expression"));
+            values.push(new Failure("Invalid Expression"));
         }
     }
 
-    tokens = tokens.map((token) => {
-        if (typeof token == "string"){
-            let parsedToken = Number(token);
-            return isNaN(parsedToken)?new Failure("Invalid Expression"): new Operand(parsedToken);
+    const tokens = expression.match(/[\d\.]+|[-\/\+*()\^]/g);
+    const valid = expression.match(/[^\d\.\/\+\s*()\^-]+/g)==null;
+    const values = [];
+    const operators = [];
+
+    for (let index = 0; index < tokens.length; index++) {
+        const token = tokens[index];
+        
+        if (is_Numerical(token)){
+            values.push(Number(token));
         }
-        return token;
-    });
-    return tokens;
-}
-
-function shuntingYardAlgorithm(expressionArray) {
-
-    // https://en.wikipedia.org/wiki/Shunting-yard_algorithm
-    const operatorArray = [];
-    const outputArray = [];
-
-    for (let index = 0; index < expressionArray.length; index++) {
-        const token = expressionArray[index];
-        if (token instanceof Operand || token instanceof Failure){
-            outputArray.push(token);
+        else if (token == "("){
+            operators.push(token);
         }
-        else if (token instanceof Operator){
-            if (operatorArray.length != 0){
-                const lastOperator = operatorArray.pop();
-                if (token.precedence >= lastOperator.precedence && token instanceof Operator){
-                    // At the moment left-associativity is ignored
-                    outputArray.push(lastOperator);
-                }
-                else {
-                    operatorArray.push(lastOperator);
-                }
+        else if (token == ")"){
+            let top = peek(operators);
+            while (top != null && top != "("){
+                execute(operators.pop(), values);
+                top = peek(operators);
             }
-            operatorArray.push(token);
+            operators.pop();
+
         }
         else {
-            outputArray.push(
-                new Failure(
-                    "Failed to apply the Shunting-Yard-Algorithm")
-            );
-        }
-    }
-    outputArray.push(...operatorArray);
-    return outputArray;
-}
-
-function constructTree(expressionArray){
-
-    // https://en.wikipedia.org/wiki/Binary_expression_tree
-    const stack = [];
-    expressionArray.forEach((token) => {
-        if (token instanceof Operand || token instanceof Failure){
-            stack.push(token);
-        }
-        else if (token instanceof BinaryOperator){
-            if (stack.length != 0){
-                token.right = stack.pop();
-                if (stack.length >= 1){
-                    token.left = stack.pop();
-                }
+            let top = peek(operators);
+            while (top != null && !"()".includes(top) &&
+                binaryOperatorPrecedence[top] > binaryOperatorPrecedence[token]){
+                    execute(operators.pop(), values);
+                    top = peek(operators);
             }
-            stack.push(token);
+            operators.push(token);
         }
-    });
-    
-    if (expressionArray.length==0){
-        return new Failure("");
     }
-    else{
-        return expressionArray.pop();
+
+    while (peek(operators) != null){
+        execute(operators.pop(), values);
     }
+
+    return values;
 }
 
-function evaluateExpression(expression){
-    let result = tokenize(expression);
-    result = shuntingYardAlgorithm(result);
-    result = constructTree(result);
-    result = result.evaluate();
 
-    if (result instanceof Failure){
-        return result.message;
-    }
-    else {
-        const digits = 10000;
-        result = Math.round((result + Number.EPSILON) * digits) / digits;
-        return result.toString();
-    }
-}
-
-/* const solveButton = document.querySelector(".solve-button");
+const solveButton = document.querySelector(".solve-button");
 solveButton.addEventListener("click", () => {
     console.log(document.getElementById("expression").value);
     document.getElementById("expression").value = "OK";
-}); */
+}); 
 
-
-function test(){
-
-    // unary - 
-    let x = shuntingYardAlgorithm(tokenize("--1-2-3--4"));
-    console.log(x, constructTree(x), constructTree(x).evaluate());
-    
-}
-
-test();
-
-module.exports = evaluateExpression
+console.log(solve("---1--2--3-  abc  --4 "));
+console.log(solve("   ++1 + + + 1++1+++1 "));
+console.log(solve("(1+2.001)*3/2-4^0"));
+console.log(solve("1+2.001*3"));
